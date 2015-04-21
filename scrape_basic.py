81f88dd30842aa3369c8a4e5e23599f9bbfec472
patrickallo <patrick.allo@gmail.com>
2015-04-21 08:45:46 +0200

    modified:   scrape_basic.py     added option to list_comments for also doing unstructured listing of comments     no children is now empty list instead of None

--- scrape_basic.py
+++ scrape_basic.py
@@ -9,14 +9,17 @@ req = requests.get(url)
 soup = BeautifulSoup(req.content)
 
 
-def list_comments(a_soup, depth=1):
+def list_comments(a_soup, depth=1, structured=True):
     """takes comments from bs4-resultset and yields a dict for each item"""
     output = []
     depth_search = "depth-" + str(depth)
-    if depth == 1:
-        comments = a_soup.find("ol", {"id": "commentlist"}).find_all("li", {"class": depth_search})
+    if structured:
+        if depth == 1:
+            comments = a_soup.find("ol", {"id": "commentlist"}).find_all("li", {"class": depth_search})
+        else:
+            comments = a_soup.find("ul", {"class": "children"}).find_all("li", {"class": depth_search})
     else:
-        comments = a_soup.find("ul", {"class": "children"}).find_all("li", {"class": depth_search})
+        comments = a_soup.find("ol", {"id": "commentlist"}).find_all("li")
     for comment in comments:
         try:
              website = comment.find("cite").find("a", {"rel": "external nofollow"}).get("href")
@@ -27,9 +30,9 @@ def list_comments(a_soup, depth=1):
                 newdepth = depth + 1
                 children = list_comments(comment, depth=newdepth)
             else:
-                children = None
+                children = []
         except:
-            children = None
+            children = []
         newdict = { "com-id": comment.get("id"),
                 "text": [item.text for item in comment.find_all("p")],
                 "auth": {"name": comment.find("cite").find("span").text, "homepage": website},
