abf799c3d7c4ec8209636b2c02833992021c2eac
patrickallo <patrick.allo@gmail.com>
2015-05-06 21:54:57 +0200

    Create_graph method renamed as parse_thread, and returns DiGraph and dict of comments. Methods print_nodes and print_html functional. Rudimentary draw_graph method added.
    Code cleaned up.

--- comment_thread.py
+++ comment_thread.py
@@ -7,7 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 import networkx as nx
 from datetime import datetime
-import json
+import yaml
 import matplotlib.pyplot as plt
 
 class CommentThread(object):
@@ -15,39 +15,51 @@ class CommentThread(object):
     Object that stores a comment thread to a WordPress post in a directed graph.
 
     Methods:
-        to be included
+        parse_thread: uses soup to create dict of html of comments,
+        and graph with decorated nodes and edges.
+        print_nodes: prints data of requested node out in yaml.
+        print_html: prints out html-soup of selected node.
 
     Attributes:
         url: the url of the blog-post
         req: request from url
         soup: BeautifullSoup parsing of content of request
+        comments_and_graph: dict of html of comments, and DiGraph of thread-structure.
         graph: DiGraph based on thread
     """
     def __init__(self, url):
         self.url = url
         self.req = requests.get(url)
         self.soup = BeautifulSoup(self.req.content)
-        self.graph = self.create_graph(self.soup)
+        self.comments_and_graph = self.parse_thread(self.soup)
+        self.graph = self.comments_and_graph["as_graph"]
 
-    def create_graph(self, a_soup):
-        """ Creates an nx.DiGraph from the comment_soup. This method is only used by init."""
+    @classmethod
+    def parse_thread(cls, a_soup):
+        """ Creates an nx.DiGraph from the comment_soup, and returns both soup and graph.
+        This method is only used by init."""
         a_graph = nx.DiGraph()
+        a_dict = {}
         all_comments = a_soup.find("ol", {"id": "commentlist"}).find_all("li")
         for comment in all_comments:
-            # identify id and class
+            # identify id, class, depth and content
             com_id = comment.get("id")
             com_class = comment.get("class")
             com_depth = next(int(word[-1]) for word in com_class if word.startswith("depth-"))
-            com_all_content = [item.text for item in comment.find("div", {"class":"comment-author vcard"}).find_all("p")]
+            com_all_content = [item.text for item in
+                               comment.find("div", {"class":"comment-author vcard"}).find_all("p")]
+            ## add complete html to dict
+            a_dict[com_id] = comment
             # creating timeStamp (currently as string, but should become date-object)
             time_stamp = " ".join(com_all_content[-1].split()[-7:])[2:]
             try:
                 time_stamp = datetime.strptime(time_stamp, "%B %d, %Y @ %I:%M %p")
-            except AttributeError:
-                print "datetime failed, replaced by: ", time_stamp
+            except AttributeError as err:
+                print err, ": datetime failed"
             # getting href to comment author webpage (if available)
             try:
-                com_author_url = comment.find("cite").find("a", {"rel": "external nofollow"}).get("href")
+                com_author_url = comment.find("cite").find("a",
+                                                           {"rel": "external nofollow"}).get("href")
             except AttributeError:
                 com_author_url = None
             # make list of child-comments (only id's)
@@ -75,10 +87,38 @@ class CommentThread(object):
         for (node_id, data) in a_graph.nodes_iter(data=True):
             if data["com_children"]:
                 a_graph.add_edges_from(((node_id, child) for child in data["com_children"]))
-        return a_graph
+        return {'as_dict': a_dict, 'as_graph': a_graph}
+
+    def print_nodes(self, *select):
+        """Prints out node-data as yaml. No output."""
+        if select:
+            select = self.graph.nodes() if select == "All" else select
+            for comment in select:
+                # do something if comment does not exist!
+                print "com_id:", comment
+                try:
+                    print yaml.safe_dump(self.graph.node[comment], default_flow_style=False)
+                except KeyError as err:
+                    print err, "not found"
+                print "---------------------------------------------------------------------"
+        else:
+            print "No nodes were selected"
+
+    def print_html(self, *select):
+        """Prints out html for selected comments."""
+        if select:
+            select = self.comments_and_graph["as_dict"].keys() if select == "All" else select
+            for key in select:
+                try:
+                    print self.comments_and_graph["as_dict"][key]
+                except KeyError as err:
+                    print err, "not found"
+        else:
+            print "No comment was selected"
+
+    def draw_graph(self):
+        """Draws and shows graph."""
+        nx.draw(self.graph, with_labels=True, arrows=True)
+        plt.show()
 
 A = CommentThread("http://polymathprojects.org/2012/07/12/minipolymath4-project-imo-2012-q3/")
-print json.dumps(A.graph.node['comment-7579'], sort_keys=True, indent=4)
-
-nx.draw(A.graph, with_labels=True, arrows=True)
-plt.show()
