ccda66fb9ae4bc26a41b5ff9d40fe1a05d31237f
Patrick Allo <patrick.allo@gmail.com>
2015-08-23 23:40:56 +0200

    Cleaned up main in comment_thread for cleaner handling of different types of threads.
    Added subclasses for parsing of Gowers and Tao discussion-threads to comment_thread.

--- comment_thread.py
+++ comment_thread.py
@@ -13,19 +13,18 @@ from matplotlib.dates import date2num
 import matplotlib.pyplot as plt
 import access_classes as ac
 import export_classes as ec
+import yaml
 #import matplotlib.dates as mdates
 
 def main(urls, thread_type="Polymath"):
     """Created thread based on supplied url, and draws graph."""
-    if thread_type == "Polymath":
-        the_threads = [CommentThreadPolymath(url) for url in urls]
-        an_mthread = MultiCommentThread(*the_threads)
-        #TODO: find usefull way of selecting subgraphs
-        #a_select = a_thread.graph.nodes()[5:15] # does not only select level_1 nodes!
-        an_mthread.draw_graph()
-        #a_thread.print_nodes(*a_select)
-    else:
-        print "No other types currently implemented."
+    try:
+        the_threads = eval("[CommentThread{}(url) for url in {}]".format(thread_type, urls))
+    except ValueError as err:
+        print err
+        the_threads = []
+    an_mthread = MultiCommentThread(*the_threads)
+    an_mthread.draw_graph()
 
 class CommentThread(ac.ThreadAccessMixin, object):
     """
@@ -51,8 +50,6 @@ class CommentThread(ac.ThreadAccessMixin, object):
         super(CommentThread, self).__init__()
         self.req = requests.get(url)
         self.soup = BeautifulSoup(self.req.content, 'html5lib')
-        self.post_title = self.soup.find("div", {"class": "post"}).find("h3").text
-        self.post_content = self.soup.find("div", {"class":"storycontent"}).find_all("p")
         self.comments_and_graph = self.parse_thread(self.soup)
         ## creates sub_graph and node:author dict based on comments_only
         if comments_only:
@@ -157,9 +154,11 @@ class MultiCommentThread(ac.ThreadAccessMixin, ec.GraphExportMixin, object):
 
 
 class CommentThreadPolymath(CommentThread):
-    """ Child class for PolyMath"""
+    """ Child class for PolyMath Blog"""
     def __init__(self, url, comments_only=True):
         super(CommentThreadPolymath, self).__init__(url, comments_only)
+        self.post_title = self.soup.find("div", {"class": "post"}).find("h3").text
+        self.post_content = self.soup.find("div", {"class":"storycontent"}).find_all("p")
 
     @classmethod
     def parse_thread(cls, a_soup):
@@ -169,8 +168,7 @@ class CommentThreadPolymath(CommentThread):
         a_dict = {}
         the_comments = a_soup.find("ol", {"id": "commentlist"})
         if the_comments:
-        # FIXME why does this re-use the soup instead of the_comments?
-            all_comments = a_soup.find("ol", {"id": "commentlist"}).find_all("li")
+            all_comments = the_comments.find_all("li")
         else:
             all_comments = [] # if no commentlist found
         for comment in all_comments:
@@ -232,10 +230,157 @@ class CommentThreadPolymath(CommentThread):
         return {'as_dict': a_dict, 'as_graph': a_graph}
 
 
+class CommentThreadGowers(CommentThread):
+    """ Child class for Gowers Blog"""
+    def __init__(self, url, comments_only=True):
+        super(CommentThreadGowers, self).__init__(url, comments_only)
+        # TODO Is this correct for post_title in Gowers?
+        self.post_title = self.soup.find("div", {"class": "post"}).find("h3").text
+        self.post_content = self.soup.find("div", {"class":"entry"}).find_all("p")
+
+    @classmethod
+    def parse_thread(cls, a_soup):
+        """ Creates an nx.DiGraph from the comment_soup, and returns both soup and graph.
+        This method is only used by init."""
+        a_graph = nx.DiGraph()
+        a_dict = {}
+        the_comments = a_soup.find("ol", {"class": "commentlist"})
+        if the_comments:
+            all_comments = the_comments.find_all("li")
+        else:
+            all_comments = [] # if no commentlist found
+        for comment in all_comments:
+            # identify id, class, depth and content
+            com_id = comment.get("id")
+            com_class = comment.get("class")
+            com_depth = next(int(word[6:]) for word in com_class if word.startswith("depth-"))
+            com_all_content = [item.text for item in
+                               comment.find_all("p")]
+            # getting and converting author_name
+            convert_author = {"gagika" : "Gagik Amirkhanyan"}
+            try:
+                com_author = comment.find("cite").text
+            except AttributeError as err:
+                print err, comment.find("cite")
+                com_author = "unable to resolve"
+            com_author = convert_author[com_author] if com_author in convert_author else com_author
+            ## add complete html to dict
+            a_dict[com_id] = comment
+            # creating timeStamp
+            time_stamp = comment.find("small").find("a").text
+            try:
+                time_stamp = datetime.strptime(time_stamp, "%B %d, %Y at %I:%M %p")
+            except ValueError as err:
+                print err, ": datetime failed"
+            # getting href to comment author webpage (if available)
+            try:
+                com_author_url = comment.find("cite").find("a",
+                                                           {"rel": "external nofollow"}).get("href")
+            except AttributeError:
+                com_author_url = None
+            # NOTE no child-comments in this type of thread
+            # TODO check if this is really true for all Gowers-threads
+            # creating dict of comment properties to be used as attributes of comment nodes
+            attr = {
+                "com_type" : com_class[0],
+                "com_depth" : com_depth,
+                "com_content" : com_all_content[:-1],
+                "com_timestamp" : time_stamp,
+                "com_author" : com_author,
+                "com_author_url" : com_author_url,
+                "com_children" : []} # no child_comments here
+            # adding node
+            a_graph.add_node(com_id)
+            # adding all attributes to node
+            for (key, value) in attr.iteritems():
+                a_graph.node[com_id][key] = value
+        # creating edges DISABLED
+        # TODO re-enable if needed
+        for node_id, children in nx.get_node_attributes(a_graph, "com_children").iteritems():
+            if children:
+                a_graph.add_edges_from(((node_id, child) for child in children))
+        return {'as_dict': a_dict, 'as_graph': a_graph}
+
+
+class CommentThreadTao(CommentThread):
+    """ Child class for Tao Blog"""
+    def __init__(self, url, comments_only=True):
+        super(CommentThreadTao, self).__init__(url, comments_only)
+        self.post_title = self.soup.find("div", {"class": "post-meta"}).find("h1").text
+        self.post_content = self.soup.find("div", {"class":"post-content"}).find_all("p")
+
+    @classmethod
+    def parse_thread(cls, a_soup):
+        """ Creates an nx.DiGraph from the comment_soup, and returns both soup and graph.
+        This method is only used by init."""
+        a_graph = nx.DiGraph()
+        a_dict = {}
+        the_comments = a_soup.find("div", {"class": "commentlist"})
+        if the_comments:
+            # this seems to ignore the pingbacks
+            all_comments = the_comments.find_all("div", {"class": "comment"})
+            all_comments += the_comments.find_all("div", {"class": "pingback"})
+        else:
+            all_comments = [] # if no commentlist found
+        for comment in all_comments:
+            # identify id, class, depth and content
+            com_id = comment.get("id")
+            com_class = comment.get("class")
+            com_depth = next(int(word[6:]) for word in com_class if word.startswith("depth-"))
+            com_all_content = [item.text for item in
+                               comment.find_all("p")]
+            # getting and converting author_name
+            convert_author = {"gagika" : "Gagik Amirkhanyan"}
+            try:
+                com_author = comment.find("p", {"class": "comment-author"}).text
+            except AttributeError as err:
+                print err, comment.find("cite")
+                com_author = "unable to resolve"
+            com_author = convert_author[com_author] if com_author in convert_author else com_author
+            ## add complete html to dict
+            a_dict[com_id] = comment
+            # creating timeStamp
+            time_stamp = comment.find("p", {"class": "comment-permalink"}).find("a").text
+            try:
+                time_stamp = datetime.strptime(time_stamp, "%d %B, %Y at %I:%M %p")
+            except ValueError as err:
+                print err, ": datetime failed"
+            # getting href to comment author webpage (if available)
+            try:
+                com_author_url = comment.find("p",
+                                              {"class": "comment-author"}).find("a").get("href")
+            except AttributeError:
+                com_author_url = None
+            # NOTE no child-comments in this type of thread
+            # TODO check if this is really true for all Tao-threads
+            # creating dict of comment properties to be used as attributes of comment nodes
+            attr = {
+                "com_type" : com_class[0],
+                "com_depth" : com_depth,
+                "com_content" : com_all_content[:-1],
+                "com_timestamp" : time_stamp,
+                "com_author" : com_author,
+                "com_author_url" : com_author_url,
+                "com_children" : []} # no child_comments here
+            print yaml.safe_dump(attr, default_flow_style=False)
+            # adding node
+            a_graph.add_node(com_id)
+            # adding all attributes to node
+            for (key, value) in attr.iteritems():
+                a_graph.node[com_id][key] = value
+        # creating edges DISABLED
+        # TODO re-enable if needed
+        for node_id, children in nx.get_node_attributes(a_graph, "com_children").iteritems():
+            if children:
+                a_graph.add_edges_from(((node_id, child) for child in children))
+        return {'as_dict': a_dict, 'as_graph': a_graph}
+
+
 if __name__ == '__main__':
     ARGUMENTS = sys.argv[1:]
     if ARGUMENTS:
         main(ARGUMENTS)
     else:
-        print "testing with Minipolymath 4"
-        main(['http://polymathprojects.org/2012/07/12/minipolymath4-project-imo-2012-q3/'])
+        print "testing with Tao"
+        main(["https://terrytao.wordpress.com/2009/02/05/upper-and-lower-bounds-for-the-density-hales-jewett-problem/"],
+             thread_type="Tao")
