457b6a8d5dd433048674a02b1140b6189a73a29c
Patrick Allo <patrick.allo@gmail.com>
2016-10-13 15:50:33 +0200

    split tokenizer and stemmer

--- text_functions.py
+++ text_functions.py
@@ -5,17 +5,30 @@ Module for textprocessing functions
 import re
 import nltk
 
+def tokenize(text, notext_pattern=re.compile("[^a-zA-Z0-9]"):
+	"""
+    takes unicode-text and returns tokenized content
+    """
+	filtered_text = notext_pattern.sub(" ", text)
+    tokens = [word.lower() for sent in nltk.sent_tokenize(filtered_text)
+              for word in nltk.word_tokenize(sent)]
+    return tokens
+
+def stem(tokens):
+	"""
+	takes tokens and returns stems using SnowballStemmer
+	"""
+	stemmer = nltk.stem.snowball.SnowballStemmer("english")
+    stems = [stemmer.stem(token) for token in tokens]
+    return stems
 
 def tokenize_and_stem(text, notext_pattern=re.compile("[^a-zA-Z0-9]")):
     """
     takes unicode-text and returns tokenized and stemmed
     and just tokenized content
     """
-    filtered_text = notext_pattern.sub(" ", text)
-    tokens = [word.lower() for sent in nltk.sent_tokenize(filtered_text)
-              for word in nltk.word_tokenize(sent)]
-    stemmer = nltk.stem.snowball.SnowballStemmer("english")
-    stems = [stemmer.stem(token) for token in tokens]
+    tokens = tokenize(text, notext_pattern)
+    stems = stem(tokens)
     return tokens, stems
 
 
