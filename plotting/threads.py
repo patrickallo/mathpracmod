b3b6249023991b6eb88cef5c90386edf35a34d2e
Patrick Allo <patrick.allo@gmail.com>
2017-02-08 18:59:51 +0000

    Reorganised

--- plotting/threads.py
+++ plotting/threads.py
@@ -18,12 +18,6 @@ plot_discussion_tree = partial(plot_from_mthread, "draw_graph",
                                intervals=10)
 plot_discussion_tree.__doc__ = "Plots discussion-structure from project"
 
-plot_activity_author = partial(plot_from_mthread, "plot_activity_author",
-                               thread_type="all threads", stage=-1,
-                               intervals=1)
-plot_activity_author.__doc__ = """Plots author activity over time for project
-                                  (limited to thread_type)"""
-
 plot_activity_thread = partial(plot_from_mthread, "plot_activity_thread",
                                thread_type="all threads", stage=-1,
                                color_by="cluster",
@@ -32,11 +26,23 @@ plot_activity_thread.__doc__ = """Plots thread activity over time for project
                                   (limited to thraead_type).
                                   color_by is either cluster or author"""
 
+plot_activity_author = partial(plot_from_mthread, "plot_activity_author",
+                               thread_type="all threads", stage=-1,
+                               intervals=1)
+plot_activity_author.__doc__ = """Plots author activity over time for project
+                                  (limited to thread_type)"""
+
+plot_growth_size = partial(plot_from_mthread, "plot_growth_size",
+                           thread_type="all threads", stage=-1)
+plot_growth_size.__doc__ = "Plots barplot of comments per week."
+
 # could be extended to single mthreads to look at growth of single threads
 plot_growth = partial(plot_from_mthread, "plot_growth",
                       thread_type="all threads", stage=-1)
 plot_growth.__doc__ = "Plots growth in comments in discussion"
 
-plot_growth_size = partial(plot_from_mthread, "plot_growth_size",
-                           thread_type="all threads", stage=-1)
-plot_growth_size.__doc__ = "Plots barplot of comments per week."
+plot_comment_sizes = partial(plot_from_mthread, "plot_sizes",
+                             thread_type="all threads", stage=-1,
+                             resample="Daily")
+plot_comment_sizes.__doc__ = """Plots average wordcounts of comments per
+                                resample (default=Daily)."""
