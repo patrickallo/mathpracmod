5e31bf916944b2ec86f851d5fd31217c1470797b
Patrick Allo <patrick.allo@gmail.com>
2017-02-08 15:13:16 +0000

    Refactored using partial and methodcaller

--- plotting/threads.py
+++ plotting/threads.py
@@ -1,59 +1,42 @@
 """module with wrapper-functions to access plotting-functions from
 MultiCommentThread"""
 
+from functools import partial
+from operator import methodcaller
 from notebook_helper.access_funs import get_project_at
 
 
-def plot_activity_author(pm_frame, project,
-                         thread_type="all threads", stage=-1,
-                         intervals=1):
-    """Wrapper function for mthread.plot_activity_author
-    Plots author activity over time for project (limited to thread_type)"""
-    data = get_project_at(
-        pm_frame, project, thread_type, stage)['mthread (accumulated)']
-    data.plot_activity_author(project=project,
-                              intervals=intervals)
+def plot_from_mthread(plot_method, pm_frame, project, **kwargs):
+    """Auxiliary-function calling supplied plot_method of data"""
+    data = get_project_at(pm_frame, project, kwargs["thread_type"],
+                          kwargs["stage"])["mthread (accumulated)"]
+    methodcaller(plot_method, project=project, **kwargs)(data)
 
 
-def plot_activity_thread(pm_frame, project,
-                         thread_type="all threads", stage=-1,
-                         color_by="cluster",
-                         intervals=1):
-    """Wrapper function for mthread.plot_activity_thread
-    Plots thread activity over time for project (limited to thraead_type).
-    color_by is either cluster or author"""
-    data = get_project_at(
-        pm_frame, project, thread_type, stage)['mthread (accumulated)']
-    data.plot_activity_thread(project=project,
-                              color_by=color_by,
-                              intervals=intervals)
+plot_discussion_tree = partial(plot_from_mthread, "draw_graph",
+                               thread_type="all threads", stage=-1,
+                               intervals=10)
+plot_discussion_tree.__doc__ = "Plots discussion-structure from project"
 
+plot_activity_author = partial(plot_from_mthread, "plot_activity_author",
+                               thread_type="all threads", stage=-1,
+                               intervals=1)
+plot_activity_author.__doc__ = """Plots author activity over time for project
+                                  (limited to thread_type)"""
 
-def plot_discussion_tree(pm_frame, project,
-                         thread_type="all threads", stage=-1,
-                         intervals=10):
-    """Wrapper function for mthread.draw_graph
-    Plots structure of discussion in project"""
-    data = get_project_at(
-        pm_frame, project, thread_type, stage)['mthread (accumulated)']
-    data.draw_graph(project=project,
-                    intervals=intervals)
-
+plot_activity_thread = partial(plot_from_mthread, "plot_activity_thread",
+                               thread_type="all threads", stage=-1,
+                               color_by="cluster",
+                               intervals=1)
+plot_activity_thread.__doc__ = """Plots thread activity over time for project
+                                  (limited to thraead_type).
+                                  color_by is either cluster or author"""
 
 # could be extended to single mthreads to look at growth of single threads
-def plot_growth(pm_frame, project,
-                thread_type="all threads", stage=-1):
-    """Wrapper function for mthread.plot_growth
-    Plots growth in comments in discussion"""
-    data = get_project_at(
-        pm_frame, project, thread_type, stage)['mthread (accumulated)']
-    data.plot_growth(project=project)
+plot_growth = partial(plot_from_mthread, "plot_growth",
+                      thread_type="all threads", stage=-1)
+plot_growth.__doc__ = "Plots growth in comments in discussion"
 
-
-def plot_growth_size(pm_frame, project,
-                     thread_type="all threads", stage=-1):
-    """Wrapper function for mthread.plot_growth_size
-    Plots barplot of comments per week."""
-    data = get_project_at(
-        pm_frame, project, thread_type, stage)['mthread (accumulated)']
-    data.plot_growth_size(project=project)
+plot_growth_size = partial(plot_from_mthread, "plot_growth_size",
+                           thread_type="all threads", stage=-1)
+plot_growth_size.__doc__ = "Plots barplot of comments per week."
