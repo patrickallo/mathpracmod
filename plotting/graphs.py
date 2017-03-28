0bc634c79bb457d23439a95613336c9bbbe5595b
Patrick Allo <patrick.allo@gmail.com>
2017-03-28 08:58:54 +0200

    New module with standalone functions for drawing graphs of discussion-trees

---
+++ plotting/graphs.py
@@ -0,0 +1,154 @@
+"""
+Module with stand-alone graph-plotting functions
+"""
+
+# Imports
+from matplotlib.dates import date2num, DateFormatter, DayLocator
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import networkx as nx
+
+import access_classes as ac
+from multi_comment_thread import MultiCommentThread
+
+
+SETTINGS, CMAP = ac.load_settings()
+
+
+# Functions
+def draw_discussion_tree(mthread_or_graph, project,
+                         intervals=10, first=None, last=None,
+                         blog_nodes=None, author_color=None, node_name=None,
+                         **kwargs):
+    """Plots discussion_tree
+    Should be called with project as kwarg for correct title."""
+    _, show, _ = ac.handle_kwargs(**kwargs)
+    first = SETTINGS['first_date'] if not first else first
+    last = SETTINGS['last_date'] if not last else last
+    if isinstance(mthread_or_graph, MultiCommentThread):
+        discussion_tree = mthread_or_graph.graph
+        blog_nodes = mthread_or_graph.type_nodes
+        author_color = mthread_or_graph.author_color
+        node_name = mthread_or_graph.node_name
+    elif isinstance(mthread_or_graph, nx.classes.digraph.DiGraph):
+        discussion_tree = mthread_or_graph
+        if not (blog_nodes and author_color and node_name):
+            raise ValueError
+    else:
+        raise ValueError
+    # creating title and axes
+    figure = plt.figure()
+    figure.suptitle("Thread structure for {}".format(project).title(),
+                    fontsize=12)
+    axes = figure.add_subplot(111)
+    axes.yaxis.set_major_locator(DayLocator(interval=intervals))
+    axes.yaxis.set_major_formatter(DateFormatter('%b %d, %Y'))
+    axes.yaxis.set_ticks_position('left')
+    axes.xaxis.set_ticks(list(range(1, 11)))
+    axes.set_xlabel("Comment Levels")
+    axes.xaxis.set_ticks_position('bottom')
+    first, last, *_ = ac.check_date_type(first, last)
+    dates = sorted([data["com_timestamp"] for _, data in
+                    discussion_tree.nodes_iter(data=True)])
+    first, last = max(first, dates[0]), min(last, dates[-1])
+    plt.ylim(first, last)
+    # creating and drawingsub_graphs
+    types_markers = {thread_type: marker for (thread_type, marker) in
+                     zip(blog_nodes.keys(),
+                         ['o', '>', 'H', 'D'][:len(
+                             blog_nodes.keys())])}
+    for (thread_type, marker) in types_markers.items():
+        type_subgraph = discussion_tree.subgraph(blog_nodes[thread_type])
+        # generating colours and positions for sub_graph
+        positions = {node_id: (data["com_depth"],
+                               date2num(data["com_timestamp"]))
+                     for (node_id, data) in
+                     type_subgraph.nodes_iter(data=True)}
+        node_color = {node_id: (author_color[node_name[node_id]])
+                      for node_id in type_subgraph.nodes()}
+        # drawing nodes of type_subgraph
+        nx.draw_networkx_nodes(type_subgraph, positions,
+                               node_size=200,
+                               nodelist=list(node_color.keys()),
+                               node_color=list(node_color.values()),
+                               node_shape=marker,
+                               vmin=SETTINGS['vmin'],
+                               vmax=SETTINGS['vmax'],
+                               cmap=CMAP,
+                               ax=axes)
+        nx.draw_networkx_edges(type_subgraph, positions, width=.5)
+        # nx.draw_networkx_labels(type_subgraph, positions, fontsize=4)
+        if SETTINGS['show_labels_comments']:
+            nx.draw_networkx_labels(
+                type_subgraph, positions, font_size=8,
+                labels={node: node[9:] for node in node_color.keys()})
+    # show all
+    plt.style.use(SETTINGS['style'])
+    the_lines = [mlines.Line2D([], [], color='gray',
+                               marker=marker,
+                               markersize=5,
+                               label=thread_type[13:])
+                 for (thread_type, marker) in types_markers.items()]
+    plt.legend(title="Where is the discussion happening",
+               handles=the_lines)
+    ac.show_or_save(show)
+
+
+def draw_discussion_tree_radial(mthread_or_graph, project,
+                                intervals=10, first=None, last=None,
+                                blog_nodes=None, author_color=None,
+                                node_name=None, **kwargs):
+    """Plots discussion_tree
+    Should be called with project as kwarg for correct title."""
+    _, show, _ = ac.handle_kwargs(**kwargs)
+    first = SETTINGS['first_date'] if not first else first
+    last = SETTINGS['last_date'] if not last else last
+    if isinstance(mthread_or_graph, MultiCommentThread):
+        discussion_tree = mthread_or_graph.graph
+        blog_nodes = mthread_or_graph.type_nodes
+        author_color = mthread_or_graph.author_color
+        node_name = mthread_or_graph.node_name
+    elif isinstance(mthread_or_graph, nx.classes.digraph.DiGraph):
+        discussion_tree = mthread_or_graph
+        if not (blog_nodes and author_color and node_name):
+            raise ValueError
+    else:
+        raise ValueError
+    discussion_tree = discussion_tree.reverse(copy=True)
+    discussion_tree.add_node("root", attr_dict={'com_depth': 0})
+    for node in discussion_tree.nodes(data=True):
+        if node[1]['com_depth'] == 1:
+            discussion_tree.add_edge("root", node[0])
+    node_color = {
+        int(node_id[8:]): author_color[discussion_tree.node[
+            node_id]['com_author']] for node_id in discussion_tree.nodes() if
+        node_id != 'root'}
+    # node_color[0] = 0
+    mapping = {node: node[8:] for node in discussion_tree.nodes()}
+    mapping["root"] = 0
+    nx.relabel_nodes(discussion_tree, lambda x: int(mapping[x]), copy=False)
+    discussion_tree = nx.Graph(discussion_tree.edges())
+    tree_pos = nx.nx_pydot.graphviz_layout(discussion_tree,
+                                           prog='twopi', root=0, args='')
+    _, axes = plt.subplots()
+    axes.set_ticks
+    axes.xaxis.set_ticks([])
+    axes.yaxis.set_ticks([])
+    axes.set_title("Thread structure for {}".format(project).title())
+    nx.draw_networkx_edges(discussion_tree, tree_pos, width=.5, alpha=.3)
+    # nx.draw_networkx_labels(discussion_tree, tree_pos, fontsize=4)
+    nx.draw_networkx_nodes(discussion_tree.subgraph(0), {0: tree_pos.pop(0)},
+                           node_size=150,
+                           node_list=[0],
+                           node_color='k',
+                           ax=axes)
+    nx.draw_networkx_nodes(discussion_tree.subgraph(node_color.keys()),
+                           tree_pos,
+                           node_size=150,
+                           nodelist=list(node_color.keys()),
+                           node_color=list(node_color.values()),
+                           vmin=SETTINGS['vmin'],
+                           vmax=SETTINGS['vmax'],
+                           cmap=CMAP, ax=axes)
+    plt.style.use(SETTINGS['style'])
+    ac.show_or_save(show)
