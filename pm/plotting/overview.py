4c3cc1ca457ad0d21f830edf8dedeb82bbed4371
Patrick Allo <patrick.allo@gmail.com>
2018-01-30 14:27:57 +0100

    Fixed annotations

--- pm/plotting/overview.py
+++ pm/plotting/overview.py
@@ -76,7 +76,7 @@ def plot_overview(pm_frame, annotate=True):
     axes.set_yticks(range(0, 200, 25))
     if annotate:
         y_values = author_data.sum(axis=1).loc[
-            ["Polymath {}".format(i) for i in [1, 4, 5, 8, 11]]].values
+            ["Polymath {}".format(i) for i in [1, 4, 5, 8, 11, 13]]].values
         axes.annotate(
             'published', xy=(0, y_values[0]), xytext=(0, y_values[0] + 20),
             arrowprops=dict(facecolor='steelblue', shrink=0.05),
@@ -95,6 +95,11 @@ def plot_overview(pm_frame, annotate=True):
         axes.annotate(
             'closed with partial results', xy=(10, y_values[4]),
             xytext=(10, y_values[4] + 20),
+            arrowprops=dict(facecolor='steelblue', shrink=0.05),
+            horizontalalignment='center')
+        axes.annotate(
+            'first success booked', xy=(12, y_values[5]),
+            xytext=(12, y_values[5] + 20),
             arrowprops=dict(facecolor='steelblue', shrink=0.05),
             horizontalalignment='center')
     comment_data = np.sqrt(comment_data)
